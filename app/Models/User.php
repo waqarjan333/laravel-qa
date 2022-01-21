0fbac8f2981561fe9d7d8e64b5994f7e02a2bbe7
waqarjan333 <waqar@integet.com>
2022-01-21 21:19:54 +0500

    Question and user relatinship

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function questions(){
+        return $this->hasMany(Question::class);
+    }
+
+    public function setTitleAttribute($values){
+        $attribute['title'] = $values;
+        $attribute['slug'] = str_slug($values);
+    }
 }
